e83d9ef32bb6caffd7a7f01ca8a4bca8aaf654c3
Gabe Guerrero <gguerrero11@gmail.com>
2020-04-24 08:31:54 -0600

    initial commit

---
+++ GithubSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubSearch
+//
+//  Created by Gabe Guerrero on 4/24/20.
+//  Copyright © 2020 Gabriel Guerrero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
