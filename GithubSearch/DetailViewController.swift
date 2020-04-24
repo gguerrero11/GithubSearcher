1c8cfac881147ad4cc2e03a1985e9fafd6485a82
Gabe Guerrero <gguerrero11@gmail.com>
2020-04-24 09:46:09 -0600

    storyboard design

--- GithubSearch/DetailViewController.swift
+++ GithubSearch/DetailViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  DetailViewController.swift
 //  GithubSearch
 //
 //  Created by Gabe Guerrero on 4/24/20.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class DetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
